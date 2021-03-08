9881a26ab29d20dfe7e174a759fae3ca256f5fd9
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-03-08 15:35:10 -0500

    initial commit

---
+++ GrpcBandwagon/ClientWorker.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcBandwagon
+{
+    public class ClientWorker : BackgroundService
+    {
+        private readonly ILogger<ClientWorker> _logger;
+        private readonly string _url;
+
+        public ClientWorker(ILogger<ClientWorker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _url = configuration["Kestrel:Endpoints:Http:Url"];
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var channel = GrpcChannel.ForAddress(_url);
+            var client = new Greeter.GreeterClient(channel);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var reply = await client.SayHelloAsync(new HelloRequest
+                {
+                    Name = "Worker"
+                });
+
+                _logger.LogInformation($"Greeting: {reply.Message} -- {DateTime.Now}");
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ GrpcBandwagon/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GrpcBandwagon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    /*
+                     * Only listens on port 5000 and HTTP
+                     * because HTTP2 and TLS are not supported
+                     * on macOS. See the appSettings.json for
+                     * the url and port number.
+                     */
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // could be anywhere
+                    services.AddHostedService<ClientWorker>();
+                });
+    }
+}
